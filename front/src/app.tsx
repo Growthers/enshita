ab2123d3e24ec8e3307949ab4486a163472b2c11
koutyuke <75959529+koutyuke@users.noreply.github.com>
2022-08-17 03:15:49 +0900

    fix(front): change reset.css (normalize.css -> destyle.css)

--- front/src/app.tsx
+++ front/src/app.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Layout from "~/components/layout";
 import Router from "./routes";
 import "~/styles/global.scss";
-import "normalize.css";
+import "destyle.css";
 
 const App = () => (
   <React.StrictMode>
