c951557e222729fb9770dc4a93de276434f9670b
koutyuke <75959529+koutyuke@users.noreply.github.com>
2022-08-18 00:55:09 +0900

    fix(front): delete 'Layout'

--- front/src/app.tsx
+++ front/src/app.tsx
@@ -1,14 +1,11 @@
 import React from "react";
-import Layout from "~/components/layout";
 import Router from "./routes";
 import "~/styles/global.scss";
 import "destyle.css";
 
 const App = () => (
   <React.StrictMode>
-    <Layout>
-      <Router />
-    </Layout>
+    <Router />
   </React.StrictMode>
 );
 
