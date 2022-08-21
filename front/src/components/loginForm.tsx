bba32c487c8fb047ab4c37c77d7a3a1b59596fa0
koutyuke <75959529+koutyuke@users.noreply.github.com>
2022-08-21 23:26:29 +0900

    fix(front): change 'axios' and delte 'useState'

--- front/src/components/loginForm.tsx
+++ front/src/components/loginForm.tsx
@@ -5,13 +5,12 @@ import { AiOutlineUser } from "react-icons/ai";
 import { HiLockClosed } from "react-icons/hi";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
-import axios from "axios";
+import { client } from "~/libs/apiClient";
 
 type FormDataProps = {
   emailOrName: string;
   password: string;
 };
-
 
 const schema = z.object({
   emailOrName: z.string().min(1),
@@ -24,22 +23,20 @@ const LoginForm: FC = () => {
     handleSubmit,
     formState: { errors },
   } = useForm<FormDataProps>({ resolver: zodResolver(schema) });
-  const [IsEmailOrName, setEmailOrName] = useState(false);
-  const [IsPassword, setPassword] = useState(false);
+
   const [IsNorFound, setNotFound] = useState(false);
 
   const onSubmit: SubmitHandler<FormDataProps> = data => {
-    axios
+    client
       .post("", {
         mail: data.emailOrName,
         password: data.password,
       })
-      .then( () => {
-      })
-      .catch( () => {
-        setNotFound(true)
+      .then(() => {})
+      .catch(() => {
+        setNotFound(true);
       });
-};
+  };
 
   return (
     <div className={styles["enshita-loginform-body"]}>
@@ -53,30 +50,14 @@ const LoginForm: FC = () => {
           <div className={styles["enshita-loginform-form-body"]}>
             <label htmlFor="email/name">
               <small>Email Address / Username</small>
-              <div
-                className={`${styles["enshita-loginform-textarea"]} ${
-                  styles[
-                    IsEmailOrName
-                      ? "enshita-isvalue-true"
-                      : "enshita-isvalue-false"
-                  ]
-                }`}
-              >
+              <div className={styles["enshita-loginform-textarea"]}>
                 <div className={styles["enshita-loginform-icon"]}>
                   <AiOutlineUser size={20} />
                 </div>
                 <input
                   className={styles["enshita-loginform-input"]}
                   /* eslint-disable-next-line react/jsx-props-no-spreading */
-                  {...register("emailOrName", {
-                    required: true,
-                    onBlur: () => {
-                      setEmailOrName(false);
-                    },
-                  })}
-                  onFocus={() => {
-                    setEmailOrName(true);
-                  }}
+                  {...register("emailOrName", { required: true })}
                 />
               </div>
               <div className={styles["enshita-loginform-error"]}>
@@ -88,40 +69,34 @@ const LoginForm: FC = () => {
           <div className={styles["enshita-loginform-form-body"]}>
             <label htmlFor="password">
               <small>Password</small>
-              <div
-                className={`${styles["enshita-loginform-textarea"]} ${
-                  styles[
-                    IsPassword
-                      ? "enshita-isvalue-true"
-                      : "enshita-isvalue-false"
-                  ]
-                }`}
-              >
+              <div className={styles["enshita-loginform-textarea"]}>
                 <div className={styles["enshita-loginform-icon"]}>
                   <HiLockClosed size={20} />
                 </div>
                 <input
                   className={styles["enshita-loginform-input"]}
                   /* eslint-disable-next-line react/jsx-props-no-spreading */
-                  {...register("password", {
-                    required: true,
-                    onBlur: () => {
-                      setPassword(false);
-                    },
-                  })}
-                  onFocus={() => {
-                    setPassword(true);
-                  }}
+                  {...register("password", { required: true })}
                 />
               </div>
               <div className={styles["enshita-loginform-error"]}>
-                {errors.password && <small>this field is required</small>}
-                <p className={styles[IsNorFound?"enshita-loginform-block":"enshita-loginform-none"]}><small >not found account</small></p>
+                {errors.password && (
+                  <p>
+                    <small>this field is required</small>
+                  </p>
+                )}
+                {IsNorFound && !errors.password && !errors.emailOrName && (
+                  <p>
+                    <small>not found account</small>
+                  </p>
+                )}
               </div>
             </label>
           </div>
           <div className={styles["enshita-loginform-button"]}>
-            <button type="submit" onClick={()=>setNotFound(false)}>login</button>
+            <button type="submit" onClick={() => setNotFound(false)}>
+              login
+            </button>
           </div>
         </form>
       </div>
