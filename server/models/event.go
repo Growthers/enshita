cb749bcdb8d0ae5a01376064fe038d64137af0ce
Sigumaa <66453922+Sigumaa@users.noreply.github.com>
2022-08-26 11:28:52 +0900

    fix: startdate enddate

--- server/models/event.go
+++ server/models/event.go
@@ -5,7 +5,8 @@ import "time"
 type Event struct {
 	ID          string    `json:"id"          gorm:"column:id;primaryKey;not null"`
 	Title       string    `json:"title"       gorm:"column:title;type:varchar(128);not null"`
-	Date        time.Time `json:"date"        gorm:"column:date;type:date;not null"`
+	StartDate   time.Time `json:"startDate"   gorm:"column:startdate;type:date;not null"`
+	EndDate     time.Time `json:"endDate"     gorm:"column:enddate;type:date;not null"`
 	Venue       string    `json:"venue"       gorm:"column:venue;type:varchar(256);not null"`
 	Description string    `json:"description" gorm:"column:description;type:text;not null"`
 	OGP         string    `json:"ogp"         gorm:"column:ogp;type:varchar(256);not null"`
