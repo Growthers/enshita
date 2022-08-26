c762452001bae1374b260ce25bd9e925318701ec
koutyuke <75959529+koutyuke@users.noreply.github.com>
2022-08-27 02:15:17 +0900

    feat(front): add route

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Route, Switch } from "wouter";
-import { About, Home } from "./pages";
+import { About, Home, AccountInfo } from "./pages";
 
 const Router: React.FC = () => (
   <Switch>
     <Route path="/" component={Home} />
     <Route path="/about" component={About} />
+    <Route path="/account/info" component={AccountInfo} />
   </Switch>
 );
 
