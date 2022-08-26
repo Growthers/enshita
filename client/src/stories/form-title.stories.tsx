7549a7ba2b3f3be3bfb715dcbe7bd0f3e972de1e
Rintaro Itokawa <me@re-taro.dev>
2022-08-27 01:32:14 +0900

    fix(front): Fix 66c984befd98efcea78037e8b81cc188627b16a5
    I forgot to move it.

---
+++ client/src/stories/form-title.stories.tsx
@@ -0,0 +1,13 @@
+import type { ComponentStoryObj, ComponentMeta } from "@storybook/react";
+import { Title } from "~/components/form/title";
+
+type T = typeof Title;
+type Story = ComponentStoryObj<T>;
+
+const data = {
+  title: "共同開発鯖LT #4",
+};
+
+export default { args: { title: data.title }, component: Title } as ComponentMeta<T>;
+
+export const Default: Story = {};
---
+++ client/src/tests/form-title.test.tsx
@@ -0,0 +1,21 @@
+import { composeStories } from "@storybook/testing-react";
+import "@testing-library/jest-dom";
+import { render } from "@testing-library/react";
+import * as stories from "../stories/form-title.stories";
+
+const { Default } = composeStories(stories);
+
+const options = {
+  name: "共同開発鯖LT #4"
+}
+
+describe("(components) form-title", () => {
+  test("to be [role=heading]", () => {
+    const { getByRole } = render(<Default />);
+    expect(getByRole("heading", options));
+  });
+  test("take snap shot", () => {
+    const { container } = render(<Default />);
+    expect(container).toMatchSnapshot();
+  });
+});
