9885cc1d160b2c64fa448d5c454bbe1822a41283
Rintaro Itokawa <me@re-taro.dev>
2022-08-27 00:58:48 +0900

    feat(front): Create button and anchor button component
    I want to override with as propeties but I can't so there is to
    components.
    
    Refs: #31

---
+++ client/src/components/button/anchor-button.tsx
@@ -0,0 +1,46 @@
+import type { FC } from "react";
+import { InlineIcon } from "@iconify/react";
+import styles from "~/styles/anchor-button.module.scss";
+import type {
+  ButtonIconProperties,
+  AnchorButtonProperties,
+} from "./type/model";
+
+const ButtonIcon: FC<ButtonIconProperties> = ({ icon, css, ...rest }) => (
+  <div className={`${styles["enshita-button-icon-box"]} ${css}`}>
+    <InlineIcon
+      icon={icon}
+      className={styles["enshita-button-icon"]}
+      {...rest}
+    />
+  </div>
+);
+
+const AnchorButton: FC<AnchorButtonProperties> = ({
+  children,
+  icon,
+  variant,
+  iconStyles,
+  boxStyles,
+  textStyles,
+  href,
+  ...rest
+}) => (
+  <a
+    className={`${styles["enshita-button-box"]} ${boxStyles}`}
+    href={href}
+    target="_blank"
+    rel="noopener noreferrer"
+    role="button"
+    {...rest}
+  >
+    {variant === "icon" && <ButtonIcon icon={icon} css={iconStyles} />}
+    {children && (
+      <p className={`${styles["enshita-button-text"]} ${textStyles}`}>
+        {children}
+      </p>
+    )}
+  </a>
+);
+
+export { AnchorButton };
