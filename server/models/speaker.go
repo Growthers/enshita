5047181c8788b63a7f059ac9123883c4c61b02eb
sigumaa <66453922+Sigumaa@users.noreply.github.com>
2022-08-27 22:51:26 +0900

    fix: EMail to Email

--- server/models/speaker.go
+++ server/models/speaker.go
@@ -4,7 +4,7 @@ type Speaker struct {
 	ID               string             `gorm:"column:id;primaryKey;not null"`
 	Title            string             `gorm:"column:title;type:varchar(128);not null"`
 	Name             string             `gorm:"column:name;type:varchar(128);not null"`
-	EMail            string             `gorm:"column:email;type:varchar(128);not null"`
+	Email            string             `gorm:"column:email;type:varchar(128);not null"`
 	Duration         int                `gorm:"column:duration;type:int;not null"`
 	SpeakingOrder    int                `gorm:"column:speakingOrder;type:int;not null"`
 	Event            []Event            `gorm:"column:eventId;foreignKey:ID;not null"`
