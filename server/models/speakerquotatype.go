d93549919f8121fc481757c053496c004e438748
Sigumaa <66453922+Sigumaa@users.noreply.github.com>
2022-08-27 16:30:05 +0900

    fix: speakerquotatypes -> speakerquotatype

--- server/models/speakerquotatype.go
+++ server/models/speakerquotatype.go
@@ -1,6 +1,6 @@
 package models
 
-type SpeakerQuotaTypes struct {
+type SpeakerQuotaType struct {
 	ID               string  `json:"id"               gorm:"column:id;primaryKey;not null"`
 	Name             string  `json:"name"             gorm:"column:name;type:varchar(128);not null"`
 	SpeakingDuration int     `json:"speakingDuration" gorm:"column:speakingDuration;type:int;not null"`
