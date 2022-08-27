1ddef4a111b5440774fa7bce0ac16e7c89b75d91
sigumaa <66453922+Sigumaa@users.noreply.github.com>
2022-08-27 22:03:02 +0900

    feat: add speaker model

---
+++ server/models/speaker.go
@@ -0,0 +1,12 @@
+package models
+
+type Speaker struct {
+	ID               string             `gorm:"column:id;primaryKey;not null"`
+	Title            string             `gorm:"column:title;type:varchar(128);not null"`
+	Name             string             `gorm:"column:name;type:varchar(128);not null"`
+	Mail             string             `gorm:"column:mail;type:varchar(128);not null"`
+	Duration         int                `gorm:"column:duration;type:int;not null"`
+	SpeakingOrder    int                `gorm:"column:speakingOrder;type:int;not null"`
+	Event            []Event            `gorm:"column:eventId;foreignKey:ID;not null"`
+	SpeakerQuotaType []SpeakerQuotaType `gorm:"column:speakerQuotaTypeId;foreignKey:ID;not null"`
+}
