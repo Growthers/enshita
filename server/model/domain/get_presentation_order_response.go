aa3b782112255310e68ddbcc52f82b8dbe38ebd8
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-08-29 02:28:54 +0900

    feat: add GetPresentationOrderResponse domain

---
+++ server/model/domain/get_presentation_order_response.go
@@ -0,0 +1,10 @@
+package domain
+
+import "time"
+
+type GetPresentationOrderResponse []struct {
+	ID       string    `json:"id"`
+	Name     string    `json:"name"`
+	Title    string    `json:"title"`
+	Duration time.Time `json:"duration"`
+}
