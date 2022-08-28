482d13282427702b7a0fc4b6714e2d495ad12da9
Tatsuto Yamamoto <laminne33569@approvers.dev>
2022-08-28 23:45:59 +0900

    chore: 構造体のフィールド名を小文字にしていた

--- server/models/entities/layout.go
+++ server/models/entities/layout.go
@@ -4,5 +4,5 @@ type Layout struct {
 	ID        string `gorm:"column:id;primaryKey;not null"`
 	ImageType int    `gorm:"column:imageType;type:integer;not null"`
 	ImageURL  string `gorm:"column:imageURL;type:varchar(256);not null"`
-	eventId   string `gorm:"column:eventId;foreignKey:ID;not null"`
+	EventId   string `gorm:"column:eventId;foreignKey:ID;not null"`
 }
