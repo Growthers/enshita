328f49d3001afff311ff2d09a23f25647651c76f
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2022-08-28 23:52:30 +0900

    chore(front): add route to event

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Route, Switch } from "wouter";
-import { About, Home } from "./pages";
+import { About, Home, Event } from "./pages";
 
 const Router: React.FC = () => (
   <Switch>
     <Route path="/" component={Home} />
     <Route path="/about" component={About} />
+
+    {/* event idがないときはindexを表示する */}
+    <Route path="/event" component={Home} />
+    <Route path="/event/:id">{params => <Event eventId={params.id} />}</Route>
   </Switch>
 );
 
