c9448129b7c4e95ca030624ab60fd3d25c4b9d5c
Tatsuto YAMAMOTO <me@laminne33569.net>
2022-08-29 00:02:40 +0900

    Update server/models/entities/layout.go
    Co-authored-by: Sigumaa <66453922+Sigumaa@users.noreply.github.com>

--- server/models/entities/layout.go
+++ server/models/entities/layout.go
@@ -4,5 +4,5 @@ type Layout struct {
 	ID        string `gorm:"column:id;primaryKey;not null"`
 	ImageType int    `gorm:"column:imageType;type:integer;not null"`
 	ImageURL  string `gorm:"column:imageURL;type:varchar(256);not null"`
-	EventId   string `gorm:"column:eventId;foreignKey:ID;not null"`
+	EventID   string `gorm:"column:eventId;foreignKey:ID;not null"`
 }
