16644417fa4f1b21673dd4871766113aefe0b99d
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2022-08-28 23:46:31 +0900

    chore(front): add functions about date

---
+++ client/src/tests/utils/date.test.tsx
@@ -0,0 +1,124 @@
+import { describe, expect, test } from "vitest";
+import {
+  fillzero,
+  str2Date,
+  getDateInfo,
+  getUNIXTime,
+  getDateDiff,
+} from "~/utils/date";
+
+describe("fillzero", () => {
+  test("5: number -> 05: string", () => {
+    const result = fillzero(5, 2);
+    expect(result).toBe("05");
+  });
+
+  test("2022: number -> 2022: string", () => {
+    const result = fillzero(2022, 4);
+    expect(result).toBe("2022");
+  });
+});
+
+describe("str2Date", () => {
+  test("19700101T090000+0900", () => {
+    const result = str2Date("19700101T090000+0900");
+    expect(result).toEqual(new Date("1970-01-01T09:00:00+09:00"));
+  });
+
+  test("1970-01-01T09:00:00+09:00", () => {
+    const result = str2Date("1970-01-01T09:00:00+09:00");
+    expect(result).toEqual(new Date("1970-01-01T09:00:00+09:00"));
+  });
+
+  test("20220401T123000Z", () => {
+    const result = str2Date("20220401T123000Z");
+    expect(result).toEqual(new Date("2022-04-01T12:30:00Z"));
+  });
+
+  test("2022-04-01T12:30:00Z", () => {
+    const result = str2Date("2022-04-01T12:30:00Z");
+    expect(result).toEqual(new Date("2022-04-01T12:30:00Z"));
+  });
+});
+
+describe("getDateInfo", () => {
+  test("1970-01-01T09:00:00+09:00", () => {
+    const result = getDateInfo(new Date("1970-01-01T09:00:00+09:00"));
+    expect(result).toEqual({
+      year: 1970,
+      month: 1,
+      day: 1,
+      hour: 9,
+      minute: 0,
+      second: 0,
+      dayOfWeek: "Thu",
+      strYear: "1970",
+      strMonth: "01",
+      strDay: "01",
+      strHour: "09",
+      strMinute: "00",
+      strSecond: "00",
+    });
+  });
+
+  test("2022-04-01T12:30:00Z", () => {
+    const result = getDateInfo(new Date("2022-04-01T12:30:00Z"));
+    expect(result).toEqual({
+      year: 2022,
+      month: 4,
+      day: 1,
+      hour: 21,
+      minute: 30,
+      second: 0,
+      dayOfWeek: "Fri",
+      strYear: "2022",
+      strMonth: "04",
+      strDay: "01",
+      strHour: "21",
+      strMinute: "30",
+      strSecond: "00",
+    });
+  });
+});
+
+describe("getUNIXTime", () => {
+  test("1970-01-01T09:00:00+09:00", () => {
+    const result = getUNIXTime(new Date("1970-01-01T09:00:00+09:00"));
+    expect(result).toBe(0);
+  });
+
+  test("2022-04-01T12:30:00Z", () => {
+    const result = getUNIXTime(new Date("2022-04-01T12:30:00Z"));
+    expect(result).toBe(1648816200);
+  });
+});
+
+describe("getDateDiff", () => {
+  test("2022-04-01T12:30:00Z - 1970-01-01T09:00:00+09:00", () => {
+    const result = getDateDiff(
+      new Date("1970-01-01T09:00:00+09:00"),
+      new Date("2022-04-01T12:30:00Z"),
+    );
+    expect(result).toEqual({
+      diff: 1648816200,
+      second: 0,
+      minute: 30,
+      hour: 12,
+      day: 19083,
+    });
+  });
+
+  test("1970-01-01T09:00:00+09:00 - 2022-04-01T12:30:00Z", () => {
+    const result = getDateDiff(
+      new Date("2022-04-01T12:30:00Z"),
+      new Date("1970-01-01T09:00:00+09:00"),
+    );
+    expect(result).toEqual({
+      diff: 1648816200,
+      second: 0,
+      minute: 30,
+      hour: 12,
+      day: 19083,
+    });
+  });
+});
---
+++ client/src/utils/date.ts
@@ -0,0 +1,90 @@
+export const fillzero = (num: number, digit: number) =>
+  `${"0".repeat(digit)}${num}`.slice(-1 * digit);
+
+// ISO8601形式の文字列を日付型に変換
+export const str2Date = (str: string) => {
+  // 19700101T090000+0900 の可能性があるので
+  // 1970-01-01T09:00:00+09:00への変換
+
+  try {
+    const data = str.split("T");
+    const isUTC = str.endsWith("Z");
+
+    const strDate = data[0].split("-").join("");
+    const strTime = data[1].split(":").join("").split("+").join("");
+
+    const year = strDate.slice(0, 4);
+    const month = strDate.slice(4, 6);
+    const day = strDate.slice(6, 8);
+    const hour = strTime.slice(0, 2);
+    const minute = strTime.slice(2, 4);
+    const second = strTime.slice(4, 6);
+
+    const timezoneHour = isUTC ? "" : strTime.slice(6, 8);
+    const timezoneMinute = isUTC ? "" : strTime.slice(8, 10);
+    const timezone = isUTC ? "Z" : `+${timezoneHour}:${timezoneMinute}`;
+
+    const datetime = `${year}-${month}-${day}T${hour}:${minute}:${second}${timezone}`;
+
+    return new Date(datetime);
+  } catch (e) {
+    return null;
+  }
+};
+
+export const getDateInfo = (date: Date) => {
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const hour = date.getHours();
+  const minute = date.getMinutes();
+  const second = date.getSeconds();
+  const dayOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"][
+    date.getDay()
+  ];
+
+  const data = {
+    year,
+    month,
+    day,
+    hour,
+    minute,
+    second,
+    dayOfWeek,
+    strYear: fillzero(year, 4),
+    strMonth: fillzero(month, 2),
+    strDay: fillzero(day, 2),
+    strHour: fillzero(hour, 2),
+    strMinute: fillzero(minute, 2),
+    strSecond: fillzero(second, 2),
+  };
+
+  return data;
+};
+
+export const getUNIXTime = (date: Date) => Math.floor(date.getTime() / 1000);
+
+export const getDateDiff = (date1: Date, date2: Date) => {
+  const diff = Math.abs(getUNIXTime(date1) - getUNIXTime(date2));
+
+  // diffを日にち、時間、分、秒に変換する
+  const second = diff % 60;
+  const minute = Math.floor((diff % 3600) / 60);
+  const hour = Math.floor((diff % 86400) / 3600);
+  const day = Math.floor(diff / 86400);
+
+  const data = {
+    diff,
+    second,
+    minute,
+    hour,
+    day,
+  };
+
+  return data;
+};
+
+export const str2DateInfo = (str: string) => {
+  const date = str2Date(str);
+  return date ? getDateInfo(date) : null;
+};
