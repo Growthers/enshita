3e1fa0820afb4ac0c05d2447bf1a80706ca6b406
sigumaa <66453922+Sigumaa@users.noreply.github.com>
2022-08-30 00:24:57 +0900

    fix: application entity

--- server/model/entity/application.go
+++ server/model/entity/application.go
@@ -6,7 +6,7 @@ import (
 
 type Application struct {
 	ID       string    `gorm:"column:id;primaryKey;not null"`
-	DeadLine time.Time `gorm:"column:deadLine;type:date;not null"`
+	Deadline time.Time `gorm:"column:deadLine;type:date;not null"`
 	Status   int       `gorm:"column:status;type:int;not null"`
 
 	EventID string `gorm:"column:eventId;not null"`
