ebf57d41384b360ce7f560e621b93d2bebea3617
koutyuke <75959529+koutyuke@users.noreply.github.com>
2022-08-29 19:45:30 +0900

    feat(front): add button

--- client/src/components/layout/footer.tsx
+++ client/src/components/layout/footer.tsx
@@ -8,8 +8,20 @@ const Footer: React.FC = () => (
         <div
           className={`${styles["enshita-footer-logo"]} ${styles["enshita-footer-hidden"]}`}
         >
-          <svg width="140"  height="50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 140 50">
-            <text x="0" y="40" fontSize={40} className={styles["enshita-footer-svg"]}>enshita</text>
+          <svg
+            width="140"
+            height="50"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 140 50"
+          >
+            <text
+              x="0"
+              y="40"
+              fontSize={40}
+              className={styles["enshita-footer-svg"]}
+            >
+              enshita
+            </text>
           </svg>
         </div>
         <div className={styles["enshita-footer-menu"]}>
--- client/src/components/layout/header.tsx
+++ client/src/components/layout/header.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "~/styles/components/layout/header.module.scss";
+import { AnchorButton } from "../button/anchor-button";
 
 const Header: React.FC = () => (
   <header
@@ -7,11 +8,33 @@ const Header: React.FC = () => (
     role="banner"
   >
     <div className={styles["enshita-header-logo"]}>
-      <svg width="140"  height="50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 140 50">
-        <text x="0" y="40" fontSize={40} className={styles["enshita-header-svg"]}>enshita</text>
-      </svg>
+      <a href="./" rel="noopener noreferrer">
+        <svg
+          width="140"
+          height="50"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 140 50"
+        >
+          <text
+            x="0"
+            y="40"
+            fontSize={40}
+            className={styles["enshita-header-svg"]}
+          >
+            enshita
+          </text>
+        </svg>
+      </a>
     </div>
-    <div className={styles["enshita-header-hidden"]}>button</div>
+    <div className={styles["enshita-header-hidden"]}>
+      <AnchorButton
+        variant="normal"
+        href="./login"
+        boxStyles={styles["enshita-header-button"]}
+      >
+        login
+      </AnchorButton>
+    </div>
   </header>
 );
 
