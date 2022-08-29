1f4e0fcfea82119b43cd83cad6161c33e510d230
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-08-30 03:52:45 +0900

    feat: add UpdateFormInfoRequest entity

---
+++ server/model/domain/update_form_info_request.go
@@ -0,0 +1,22 @@
+package domain
+
+import "time"
+
+type UpdateFormInfoRequest struct {
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Deadline    time.Time `json:"deadline"`
+	Questions   struct {
+		Name                 string `json:"name"`
+		Email                string `json:"email"`
+		SpeakerQuotaTypeList []struct {
+			ID           string    `json:"id"`
+			Name         string    `json:"name"`
+			Time         time.Time `json:"time"`
+			CurrentCount int       `json:"currentCount"`
+			Total        int       `json:"total"`
+		} `json:"speakerQuotaTypeList"`
+		Title     string `json:"title"`
+		Paragraph string `json:"paragraph"`
+	} `json:"questions"`
+}
