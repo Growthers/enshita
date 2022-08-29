1ab1143336b2b88bff9c5e76c38538150edaf35c
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2022-08-30 03:55:15 +0900

    fix(front): change args name and add comments on event/description component

--- client/src/components/event/description.tsx
+++ client/src/components/event/description.tsx
@@ -8,25 +8,30 @@ type Props = {
 
 const EventDescription: React.FC<Props> = props => {
   const { description } = props;
+  // descriptionの中からURLを抽出して配列に
   const urlPattern = /https?:\/\/[\w!?/+\-_~;.,*&@#$%=']+/gim;
   const URL = description.match(urlPattern) ?? [];
 
   return (
     <p className={styles["enshita-event-description-content"]}>
       {description
+        // APIからは\\nが返ってくるので\nに置き換える
         .replace(/\\n/g, "\n")
+        // URLで区切る（コンテンツの配列）
         .split(urlPattern)
-        .map((v, i) => (
-          <span key={v}>
-            {v}
-            {URL[i] && (
+        // contentとURLを交互に挿入
+        // content0, url0, content1, url1, content2...
+        .map((content, index) => (
+          <span key={content}>
+            {content}
+            {URL[index] && (
               <a
                 className={styles["enshita-event-description-link"]}
-                href={URL[i]}
+                href={URL[index]}
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                {URL[i]}
+                {URL[index]}
               </a>
             )}
           </span>
