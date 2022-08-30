72e2dfdea8cc1e402e8d4aebbe2af06f10534390
koutyuke <75959529+koutyuke@users.noreply.github.com>
2022-08-30 22:23:23 +0900

    fix(front): delete token check

--- client/src/pages/account/accountInfo.tsx
+++ client/src/pages/account/accountInfo.tsx
@@ -1,69 +1,14 @@
-import { AxiosError } from "axios";
-import { FC, useState, useEffect } from "react";
+import { FC } from "react";
 import AccountInfoForm from "~/components/form/accountInfoForm";
 import Layout from "~/components/layout/layout";
-import { client } from "~/libs/apiClient";
 import styles from "~/styles/pages/accountInfo.module.scss";
 
-type TokenCheckProps = {
-  token: string;
-  mail: string;
-  name: string;
-  password: string;
-};
-
-type PageStatusProps = {
-  status: "loading" | "failure" | "success";
-  pageData: TokenCheckProps | AxiosError | null;
-};
-
-const AccountInfo: FC = () => {
-  const params = new URLSearchParams(window.location.search);
-  const token = params.get("token");
-
-  const [{ status, pageData }, setStatus] = useState<PageStatusProps>({
-    status: "loading",
-    pageData: null,
-  });
-
-  useEffect(() => {
-    const check = () => {
-      client
-        .post<TokenCheckProps>("", {
-          token,
-        })
-        .then(({ data }) => {
-          setStatus({
-            status: "success",
-            pageData: data,
-          });
-        })
-        .catch((data: AxiosError) => {
-          setStatus({
-            status: "failure",
-            pageData: data,
-          });
-        });
-    };
-
-    check();
-  }, [token]);
-
-  if (status === "loading") {
-    return <div>loading</div>;
-  }
-  if (status === "failure" || pageData === null) {
-    // const data = pageData as AxiosError
-    return <div>page not found</div>;
-  }
-
-  return (
-    <Layout>
-      <div className={styles["enshita-body"]}>
-        <AccountInfoForm mail="example@example.com" userName="" />
-      </div>
-    </Layout>
-  );
-};
+const AccountInfo: FC = () => (
+  <Layout>
+    <div className={styles["enshita-body"]}>
+      <AccountInfoForm mail="example@example.com" userName="hoge" />
+    </div>
+  </Layout>
+);
 
 export { AccountInfo };
