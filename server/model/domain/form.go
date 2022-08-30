b0d69750e80d82d5cfb5740303b0aa156bd773b5
sigumaa <66453922+Sigumaa@users.noreply.github.com>
2022-08-31 01:20:49 +0900

    chore: change form domain

--- server/model/domain/form.go
+++ server/model/domain/form.go
@@ -6,11 +6,5 @@ type Form struct {
 	Title       string    `json:"title"`
 	Description string    `json:"description"`
 	Deadline    time.Time `json:"deadline"`
-	Questions   struct {
-		Name                 string             `json:"name"`
-		Email                string             `json:"email"`
-		SpeakerQuotaTypeList []SpeakerQuotaType `json:"speakerQuotaTypeList"`
-		Title                string             `json:"title"`
-		Paragraph            string             `json:"paragraph"`
-	} `json:"questions"`
+	Questions   Question  `json:"questions"`
 }
