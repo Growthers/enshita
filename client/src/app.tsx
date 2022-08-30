827f93a16b149801934f19b617e4768bf7d955ce
koutyuke <75959529+koutyuke@users.noreply.github.com>
2022-08-31 06:48:35 +0900

    Revert "feat(front): add react-cookie"
    This reverts commit 0fedd848303982e4cdb3d5198c1278a08fc080cc.

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,14 +1,11 @@
 import React from "react";
-import { CookiesProvider } from "react-cookie";
 import Router from "./routes";
 import "~/styles/global.scss";
 import "destyle.css";
 
 const App = () => (
   <React.StrictMode>
-    <CookiesProvider>
-      <Router />
-    </CookiesProvider>
+    <Router />
   </React.StrictMode>
 );
 
