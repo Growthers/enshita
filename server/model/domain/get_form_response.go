b0257a08a3bf8806a757e016eeeaf41327065ec8
sigumaa <66453922+Sigumaa@users.noreply.github.com>
2022-08-31 01:15:32 +0900

    chore: change get form response

--- server/model/domain/get_form_response.go
+++ server/model/domain/get_form_response.go
@@ -8,16 +8,10 @@ type GetFormResponse struct {
 	Deadline  time.Time `json:"deadline"`
 	Enable    bool      `json:"enable"`
 	Questions struct {
-		Name             string `json:"name"`
-		Email            string `json:"email"`
-		Title            string `json:"title"`
-		Paragraph        string `json:"paragraph"`
-		SpeakerQuotaType []struct {
-			ID           string    `json:"id"`
-			Name         string    `json:"name"`
-			Time         time.Time `json:"time"`
-			CurrentCount int       `json:"currentCount"`
-			Total        int       `json:"total"`
-		} `json:"speakerQuotaType"`
+		Name                 string             `json:"name"`
+		Email                string             `json:"email"`
+		Title                string             `json:"title"`
+		Paragraph            string             `json:"paragraph"`
+		SpeakerQuotaTypeList []SpeakerQuotaType `json:"speakerQuotaTypeList"`
 	}
 }
