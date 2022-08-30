66b1988daaa1952922f3df8a57da80a81e6cac91
koutyuke <75959529+koutyuke@users.noreply.github.com>
2022-08-30 15:39:39 +0900

    fix(front): change routing

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -1,12 +1,29 @@
 import React from "react";
-import { Route, Switch } from "wouter";
+import { Route, Switch, useRouter, useLocation,Router as WRouter, Link } from "wouter";
 import { About, Home, AccountInfo } from "./pages";
+
+const NestedRoutes = ({base,children}:{base:string,children:React.ReactNode}) => {
+  const router = useRouter();
+  const [parentLocation] = useLocation();
+
+  const nestedBase = `${router.base}${base}`;
+  if (!parentLocation.startsWith(nestedBase)) return null;
+
+  return(
+    <WRouter base={nestedBase} key={nestedBase}>
+      {children}
+    </WRouter>
+  )
+}
 
 const Router: React.FC = () => (
   <Switch>
     <Route path="/" component={Home} />
     <Route path="/about" component={About} />
-    <Route path="/account/info" component={AccountInfo} />
+    <NestedRoutes base="/account">
+      <Link to="/info" />
+      <Route path="/info" component={AccountInfo} />
+    </NestedRoutes>
   </Switch>
 );
 
