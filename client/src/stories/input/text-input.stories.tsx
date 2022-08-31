68d7948caa2c0274a7fff299f6971868a62a4817
Rintaro Itokawa <me@re-taro.dev>
2022-08-31 17:36:03 +0900

    chore(front): Add stories for Text Input base component

---
+++ client/src/stories/input/text-input.stories.tsx
@@ -0,0 +1,31 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { ComponentMeta, ComponentStoryObj } from "@storybook/react";
+import { TextInput } from "~/components/input/text-input";
+import { userEvent, within } from "@storybook/testing-library";
+/* eslint-enable import/no-extraneous-dependencies */
+
+type T = typeof TextInput;
+type Story = ComponentStoryObj<T>;
+
+export default {
+  args: { placeholder: "test", id: "test", "aria-label": "test", type: "text" },
+  component: TextInput,
+} as ComponentMeta<T>;
+
+export const Default: Story = {};
+
+export const Error: Story = {
+  args: {
+    error: {
+      type: "required",
+      message: "test error",
+    },
+  },
+};
+
+export const InputFieldFilled: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    await userEvent.type(canvas.getByLabelText("test"), "This is test.");
+  },
+};
