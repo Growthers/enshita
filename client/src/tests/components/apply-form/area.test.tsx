8a800a21e3ea3ad400e418f9df92f7534cb0a9d3
Rintaro Itokawa <me@re-taro.dev>
2022-09-01 03:25:16 +0900

    chore(front): Add test for text area component

---
+++ client/src/tests/components/apply-form/area.test.tsx
@@ -0,0 +1,23 @@
+import { composeStories } from "@storybook/testing-react";
+import "@testing-library/jest-dom"
+import { render } from "@testing-library/react"
+import * as stories from "~/stories/apply-form/area.stories"
+
+const { Default, Valid } = composeStories(stories)
+
+describe("(components) text area input with using react-hook-form", () => {
+  test("label should be paragraph", () => {
+    const { getByLabelText } = render(<Default />)
+    expect(getByLabelText("paragraph")).toBeInTheDocument()
+  })
+  test("(valid) input is correctly entered", async() => {
+    const { container, getByLabelText } = render(<Valid />)
+    await Valid.play({ canvasElement: container })
+    const input = getByLabelText("paragraph") as HTMLInputElement
+    expect(input.value).toEqual("abcdefghij")
+  })
+  test("take snapshot", () => {
+    const { container } = render(<Default />);
+    expect(container).toMatchSnapshot();
+  })
+})
