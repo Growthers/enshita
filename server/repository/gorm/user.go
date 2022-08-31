f65867f6404ba53e0066dfcbd88491b1ad3a9a0d
Tatsuto Yamamoto <laminne33569@approvers.dev>
2022-08-31 15:26:44 +0900

    fix: パスワードの最小文字数の設定

--- server/repository/gorm/user.go
+++ server/repository/gorm/user.go
@@ -4,10 +4,12 @@ import (
 	"encoding/base64"
 	"github.com/google/uuid"
 	"github.com/growthers/enshita/server/model/entity"
+	"github.com/growthers/enshita/server/repository"
 	"golang.org/x/crypto/argon2"
 )
 
-func (repo *Repository) CreateUser(userID, email, userName, password string, role int) (entity.User, error) {
+// CreateUser ユーザーを作成
+func (repo *Repository) CreateUser(userID, email, userName, password string, role int) (*entity.User, error) {
 	user := &entity.User{
 		Email:    email,
 		Name:     userName,
@@ -15,21 +17,24 @@ func (repo *Repository) CreateUser(userID, email, userName, password string, rol
 		Role:     role,
 	}
 
-	if len(password) >= 4 {
-		salt := "AndjefkjDSn3Nd+w$siv"
-		// ToDo: ここのパラメータの値のチューニング
-		hashed := argon2.IDKey([]byte(password), []byte(salt), 1, 6710, 2, 32)
-
-		// ハッシュされたパスワードをBase64でエンコードする
-		encodedHashedPassword := make([]byte, base64.StdEncoding.EncodedLen(len(hashed)))
-		base64.StdEncoding.Encode(encodedHashedPassword, hashed)
-		user.Password = string(encodedHashedPassword)
+	// パスワードは4文字以上である必要があります
+	if len(password) < 4 {
+		return nil, repository.RaiseError("PASSWORD_LENGTH", "password is too short")
 	}
+
+	salt := "AndjefkjDSn3Nd+w$siv"
+	// ToDo: ここのパラメータの値のチューニング
+	hashed := argon2.IDKey([]byte(password), []byte(salt), 1, 6710, 2, 32)
+
+	// ハッシュされたパスワードをBase64でエンコードする
+	encodedHashedPassword := make([]byte, base64.StdEncoding.EncodedLen(len(hashed)))
+	base64.StdEncoding.Encode(encodedHashedPassword, hashed)
+	user.Password = string(encodedHashedPassword)
 
 	// ID生成
 	u, err := uuid.NewRandom()
 	if err != nil {
-		return entity.User{}, err
+		return nil, err
 	}
 
 	user.ID = u.String()
@@ -38,8 +43,8 @@ func (repo *Repository) CreateUser(userID, email, userName, password string, rol
 	result := repo.db.Create(&user)
 
 	if result.Error != nil {
-		return entity.User{}, result.Error
+		return nil, result.Error
 	}
 
-	return *user, nil
+	return user, nil
 }
