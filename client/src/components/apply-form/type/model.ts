7b3913e30a7d71219ea06cc813c095304de0c456
Rintaro Itokawa <me@re-taro.dev>
2022-08-31 17:45:53 +0900

    feat(front): Create Text Input controller with React-hook-form

--- client/src/components/apply-form/type/model.ts
+++ client/src/components/apply-form/type/model.ts
@@ -1,3 +1,26 @@
+import type {
+  ControllerRenderProps,
+  DeepPartial,
+  FieldValues,
+} from "react-hook-form";
+import type { ComponentProps, ReactNode } from "react";
+import type { FieldByType } from "~/libs/rhf";
+import { TextInput } from "~/components/input/text-input";
+
 export type TitleProperties = {
   title: string;
+};
+
+export type WrapperProperties<T extends FieldValues = never> = {
+  defaultValues: (DeepPartial<T> | undefined) & T;
+  children: ReactNode;
+};
+
+export type InputControlProperties<T extends FieldValues> = Omit<
+  ComponentProps<typeof TextInput>,
+  "error" | "inputStyles" | keyof ControllerRenderProps
+> & {
+  label: string;
+  description?: string;
+  name: FieldByType<T, string>;
 };
