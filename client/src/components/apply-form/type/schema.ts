d68165da855b7e9506bbf83609a9ce860bc78e1c
Rintaro Itokawa <me@re-taro.dev>
2022-09-01 16:29:35 +0900

    chore(front): Update invalid message in zod

--- client/src/components/apply-form/type/schema.ts
+++ client/src/components/apply-form/type/schema.ts
@@ -3,18 +3,18 @@ import { z } from "zod";
 export const schema = z.object({
   name: z
     .string()
-    .min(1, { message: "名前は必須項目です" })
-    .max(60, { message: "60字以内で入力してください" }),
+    .min(1, { message: "名前は必須入力です。" })
+    .max(60, { message: "60字以内で入力してください。" }),
   email: z
     .string()
-    .min(1, { message: "メールアドレスは必須項目です" })
-    .email({ message: "無効なメールアドレスです" }),
-  speakerQuotaTypeId: z.string().min(1).uuid(),
+    .min(1, { message: "メールアドレスは必須入力です。" })
+    .email({ message: "無効なメールアドレスです。" }),
+  speakerQuotaTypeId: z.string().uuid({message: "登壇枠を選んでください。"}),
   title: z
     .string()
-    .min(1, { message: "タイトルは必須項目です" })
-    .max(100, { message: "100字以内で入力してください" }),
+    .min(1, { message: "タイトルは必須入力です。" })
+    .max(100, { message: "100字以内で入力してください。" }),
   paragraph: z.nullable(
-    z.string().max(3000, { message: "3000字以内で入力してください" }),
+    z.string().max(3000, { message: "3000字以内で入力してください。" }),
   ),
 });
