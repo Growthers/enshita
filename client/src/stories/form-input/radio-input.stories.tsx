f23b3f75df779032588b631f025ebe8f385913d1
Rintaro Itokawa <me@re-taro.dev>
2022-09-03 00:02:51 +0900

    chore(front): Format the code

--- client/src/stories/form-input/radio-input.stories.tsx
+++ client/src/stories/form-input/radio-input.stories.tsx
@@ -5,17 +5,15 @@ type T = typeof RadioInput;
 type Story = ComponentStoryObj<T>;
 
 export default {
-  args: { id: "test", value: "test", name: "Test", current: "2", total: "5", "aria-label": "test" },
+  args: {
+    id: "test",
+    value: "test",
+    name: "Test",
+    current: "2",
+    total: "5",
+    "aria-label": "test",
+  },
   component: RadioInput,
 } as ComponentMeta<T>;
 
 export const Default: Story = {};
-
-export const Error: Story = {
-  args: {
-    error: {
-      type: "required",
-      message: "選択する必要があります"
-    }
-  }
-}
