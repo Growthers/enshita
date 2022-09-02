2b783e9ebc3d0c5f4ef9f481d384055d88b84364
Rintaro Itokawa <me@re-taro.dev>
2022-09-02 20:25:38 +0900

    feat(front): Add radio input base component

--- client/src/components/form-input/type/model.ts
+++ client/src/components/form-input/type/model.ts
@@ -1,5 +1,5 @@
 import type { ComponentProps } from "react";
-import type { FieldError } from "react-hook-form";
+import type {FieldError} from "react-hook-form";
 
 export type TextInputProperties = ComponentProps<"input"> & {
   inputStyles?: string;
@@ -10,3 +10,13 @@ export type AreaInputProperties = ComponentProps<"textarea"> & {
   inputStyles?: string;
   error?: FieldError;
 };
+
+export type RadioInputProperties = Omit<ComponentProps<'input'>, 'id' | 'type'> & {
+  inputStyles?: string;
+  id: string;
+  value: string
+  name: string
+  current: string
+  total: string
+  error?: FieldError
+}
