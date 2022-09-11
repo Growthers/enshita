443bd61eb130e701264fccf7f9ed829fd90e30bc
koutyuke <75959529+koutyuke@users.noreply.github.com>
2022-09-11 18:58:09 +0900

    fix(front): change styles

--- client/src/pages/account/accountInfo.tsx
+++ client/src/pages/account/accountInfo.tsx
@@ -1,14 +1,11 @@
 import { FC } from "react";
 import AccountInfoForm from "~/components/account-info-form/account-info-form";
-import Layout from "~/components/layout/index";
 import styles from "~/styles/pages/accountInfo.module.scss";
 
 const AccountInfo: FC = () => (
-  <Layout>
-    <div className={styles["enshita-body"]}>
-      <AccountInfoForm mail="example@example.com" userName="hoge" />
-    </div>
-  </Layout>
+  <div className={styles["enshita-body"]}>
+    <AccountInfoForm mail="example@example.com" userName="hoge" />
+  </div>
 );
 
 export { AccountInfo };
