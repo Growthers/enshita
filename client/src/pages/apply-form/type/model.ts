f8a74a8666f3267ac87ec3a652cc8c65f37d0b5e
Rintaro Itokawa <me@re-taro.dev>
2022-09-11 14:26:09 +0900

    chore(front): Format code

--- client/src/pages/apply-form/type/model.ts
+++ client/src/pages/apply-form/type/model.ts
@@ -1,8 +1,8 @@
-import { z } from "zod"
+import { z } from "zod";
 import { schema } from "~/components/apply-form/type/schema";
 
 export type ApplyFormPageProperties = {
   eventId: string;
-}
+};
 
-export type ApplyForm = z.infer<typeof schema>
+export type ApplyForm = z.infer<typeof schema>;
