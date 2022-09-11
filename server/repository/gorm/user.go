6d187b8ea1f95eae2f898b2f67fd2847d82ca15c
Yuki Yuigishi <me@yuigishi.dev>
2022-09-12 01:29:05 +0900

    refactor(server): DI paswordEncorder

--- server/repository/gorm/user.go
+++ server/repository/gorm/user.go
@@ -3,7 +3,6 @@ package gorm
 import (
 	"github.com/google/uuid"
 	"github.com/growthers/enshita/server/model/domain"
-	"github.com/growthers/enshita/server/util"
 )
 
 // CreateUser ユーザーを作成
@@ -16,8 +15,7 @@ func (repo *Repository) CreateUser(userID, email, userName, password string, rol
 		Role:     role,
 	}
 
-	encoder := util.NewArgon2PasswordEncoder()
-	encodedPassword, err := encoder.EncodePassword(password)
+	encodedPassword, err := repo.passwordEncoder.EncodePassword(password)
 	if err != nil {
 		return nil, err
 	}
