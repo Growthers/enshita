f230274b560aa961cf4d3ba94c612627dbdc1dc7
koutyuke <75959529+koutyuke@users.noreply.github.com>
2022-09-14 21:32:54 +0900

    fix(front): add invalid valid at storybook

--- client/src/stories/account-info-form/icon-text-control.stories.tsx
+++ client/src/stories/account-info-form/icon-text-control.stories.tsx
@@ -5,6 +5,7 @@ import { FormProvider, useForm } from "react-hook-form";
 import { AccountInfoFormFieldValues } from "~/pages/account/info/type/model";
 import { accountInfoFormSchema } from "~/pages/account/info/type/schema";
 import { TextControl } from "~/components/account-info-form/icon-text-control";
+import { userEvent, within } from "@storybook/testing-library";
 
 type T = typeof TextControl;
 type Story = ComponentStoryObj<T>;
@@ -21,8 +22,7 @@ export default {
       const methods = useForm<AccountInfoFormFieldValues>({
         resolver: zodResolver(accountInfoFormSchema),
         defaultValues: {
-          mail: "fuga@hoge.com",
-          userName: "hogefuga",
+          mail: "",
         },
         mode: "onBlur",
       });
@@ -42,4 +42,36 @@ export default {
   ],
 } as ComponentMeta<T>;
 
+const type = async (step: 0 | 1 | 2, canvasElement: HTMLElement) => {
+  const canvas = within(canvasElement);
+  if (step === 0) return;
+  await userEvent.type(canvas.getByLabelText("email Address"), "fuga@hoge.com");
+  if (step === 1) return;
+  await userEvent.clear(canvas.getByLabelText("email Address"));
+};
+
+const playFactory = async (step: 0 | 1 | 2, canvasElement: HTMLElement) => {
+  const canvas = within(canvasElement);
+  await type(step, canvasElement);
+  canvas.getByLabelText("email Address").blur();
+};
+
 export const Default: Story = {};
+
+export const Nothing: Story = {
+  play: async ({ canvasElement }) => {
+    await playFactory(0, canvasElement);
+  },
+};
+
+export const Valid: Story = {
+  play: async ({ canvasElement }) => {
+    await playFactory(1, canvasElement);
+  },
+};
+
+export const Invalid: Story = {
+  play: async ({ canvasElement }) => {
+    await playFactory(2, canvasElement);
+  },
+};
--- client/src/stories/account-info-form/password-control.stories.tsx
+++ client/src/stories/account-info-form/password-control.stories.tsx
@@ -5,6 +5,7 @@ import { FormProvider, useForm } from "react-hook-form";
 import { AccountInfoFormFieldValues } from "~/pages/account/info/type/model";
 import { accountInfoFormSchema } from "~/pages/account/info/type/schema";
 import { PasswordControl } from "~/components/account-info-form/password-control";
+import { userEvent, within } from "@storybook/testing-library";
 
 type T = typeof PasswordControl;
 type Story = ComponentStoryObj<T>;
@@ -12,16 +13,15 @@ type Story = ComponentStoryObj<T>;
 export default {
   component: PasswordControl,
   args: {
-    name: "mail",
-    label: "email Address",
+    name: "newPassword",
+    label: "New Password",
   },
   decorators: [
     Story => {
       const methods = useForm<AccountInfoFormFieldValues>({
         resolver: zodResolver(accountInfoFormSchema),
         defaultValues: {
-          mail: "fuga@hoge.com",
-          userName: "hogefuga",
+          newPassword: "",
         },
         mode: "onBlur",
       });
@@ -41,4 +41,36 @@ export default {
   ],
 } as ComponentMeta<T>;
 
+const type = async (step: 0 | 1 | 2, canvasElement: HTMLElement) => {
+  const canvas = within(canvasElement);
+  if (step === 0) return;
+  await userEvent.type(canvas.getByLabelText("New Password"), "hogehoge");
+  if (step === 1) return;
+  await userEvent.clear(canvas.getByLabelText("New Password"));
+};
+
+const playFactory = async (step: 0 | 1 | 2, canvasElement: HTMLElement) => {
+  const canvas = within(canvasElement);
+  await type(step, canvasElement);
+  canvas.getByLabelText("New Password").blur();
+};
+
 export const Default: Story = {};
+
+export const Nothing: Story = {
+  play: async ({ canvasElement }) => {
+    await playFactory(0, canvasElement);
+  },
+};
+
+export const Valid: Story = {
+  play: async ({ canvasElement }) => {
+    await playFactory(1, canvasElement);
+  },
+};
+
+export const Invalid: Story = {
+  play: async ({ canvasElement }) => {
+    await playFactory(2, canvasElement);
+  },
+};
