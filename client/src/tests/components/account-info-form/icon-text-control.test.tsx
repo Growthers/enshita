14a33b2d4a728ed7ca9632d4f9c6a46079d9b4cf
koutyuke <75959529+koutyuke@users.noreply.github.com>
2022-09-14 21:33:02 +0900

    fix(front): add invalid valid test

--- client/src/tests/components/account-info-form/icon-text-control.test.tsx
+++ client/src/tests/components/account-info-form/icon-text-control.test.tsx
@@ -3,9 +3,15 @@ import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
 import * as stories from "~/stories/account-info-form/icon-text-control.stories";
 
-const { Default } = composeStories(stories);
+const { Default, Valid } = composeStories(stories);
 
 describe("(component) text control module test", () => {
+  test("(valid) input is correctly entered", async () => {
+    const { container, getByLabelText } = render(<Valid />);
+    await Valid.play({ canvasElement: container });
+    const input = getByLabelText("email Address") as HTMLInputElement;
+    expect(input.value).toEqual("fuga@hoge.com");
+  });
   test("Snap Shot", () => {
     const { container } = render(<Default />);
     expect(container).toMatchSnapshot();
--- client/src/tests/components/account-info-form/password-control.test.tsx
+++ client/src/tests/components/account-info-form/password-control.test.tsx
@@ -3,9 +3,15 @@ import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
 import * as stories from "~/stories/account-info-form/password-control.stories";
 
-const { Default } = composeStories(stories);
+const { Default, Valid } = composeStories(stories);
 
 describe("(component) password control module test", () => {
+  test("(valid) input is correctly entered", async () => {
+    const { container, getByLabelText } = render(<Valid />);
+    await Valid.play({ canvasElement: container });
+    const input = getByLabelText("New Password") as HTMLInputElement;
+    expect(input.value).toEqual("hogehoge");
+  });
   test("Snap Shot", () => {
     const { container } = render(<Default />);
     expect(container).toMatchSnapshot();
