c22f9d565474b232e69c8b04d4a1843e50963609
Rintaro Itokawa <me@re-taro.dev>
2022-09-15 23:09:04 +0900

    chore(front): Format code

--- client/src/stories/event-card/chip.stories.tsx
+++ client/src/stories/event-card/chip.stories.tsx
@@ -1,38 +1,39 @@
 import type { ComponentMeta, ComponentStoryObj } from "@storybook/react";
 import { Chip } from "~/components/event-card/chip";
 
-type T = typeof Chip
-type Story = ComponentStoryObj<T>
+type T = typeof Chip;
+type Story = ComponentStoryObj<T>;
 
 export default {
   args: {
-    status: "preparing"
-  }, component: Chip
-} as ComponentMeta<T>
+    status: "preparing",
+  },
+  component: Chip,
+} as ComponentMeta<T>;
 
-export const Defalut: Story = {}
+export const Defalut: Story = {};
 export const Open: Story = {
   args: {
-    status: "open"
-  }
-}
+    status: "open",
+  },
+};
 export const Close: Story = {
   args: {
-    status: "close"
-  }
-}
+    status: "close",
+  },
+};
 export const SuddenOpen: Story = {
   args: {
-    status: "suddenOpen"
-  }
-}
+    status: "suddenOpen",
+  },
+};
 export const SuddenClose: Story = {
   args: {
-    status: "suddenClose"
-  }
-}
+    status: "suddenClose",
+  },
+};
 export const Finish: Story = {
   args: {
-    status: "finish"
-  }
-}
+    status: "finish",
+  },
+};
