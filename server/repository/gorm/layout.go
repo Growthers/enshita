391a49a7070793a9ebd78555efc8ac8108de2673
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-09-17 02:14:07 +0900

    feat(repository): add FindLayouts

--- server/repository/gorm/layout.go
+++ server/repository/gorm/layout.go
@@ -22,3 +22,27 @@ func (repo *Repository) FindLayout(id string) (*domain.Layout, error) {
 		EventID:   l.EventID,
 	}, nil
 }
+
+// FindLayouts 特定のイベントに関する配信レイアウトを全件取得
+func (repo *Repository) FindLayouts(eventID string) ([]*domain.Layout, error) {
+	var ls []*entity.Layout
+
+	// レコード取得
+	err := repo.db.Find(&ls, "eventId = ?", eventID).Error
+	if err != nil {
+		return nil, err
+	}
+
+	// entity.Layoutをdomain.Layoutに変換
+	var dl []*domain.Layout
+	for _, l := range ls {
+		dl = append(dl, &domain.Layout{
+			ID:        l.ID,
+			ImageType: l.ImageType,
+			ImageURL:  l.ImageURL,
+			EventID:   l.EventID,
+		})
+	}
+
+	return dl, nil
+}
