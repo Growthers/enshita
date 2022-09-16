430904c53d4ab958be1c7c0d8273ec42eba124ac
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-09-17 02:28:23 +0900

    docs(controller): add godoc

--- server/controller/layout.go
+++ server/controller/layout.go
@@ -10,8 +10,10 @@ import (
 
 // ShowLayout 配信レイアウトの表示
 func (c *Controller) showLayout(ctx echo.Context) error {
+	// パスパラメータからイベントIDを取得
 	eventId := ctx.Param("id")
 
+	// イベントを取得
 	e, err := c.eventRepository.FindEvent(eventId)
 	if err != nil {
 		return err
@@ -19,6 +21,7 @@ func (c *Controller) showLayout(ctx echo.Context) error {
 		return ctx.JSON(http.StatusNotFound, nil)
 	}
 
+	// eに紐づくレイアウトを取得
 	ls, err := c.layoutRepository.FindLayouts(eventId)
 	if err != nil {
 		return err
@@ -26,6 +29,7 @@ func (c *Controller) showLayout(ctx echo.Context) error {
 		return ctx.JSON(http.StatusNotFound, nil)
 	}
 
+	// domain.Layoutをdomain.Imageに変換
 	imgs := make([]domain.Image, len(ls))
 	for i, l := range ls {
 		imgs[i].Type = strconv.Itoa(l.ImageType)
@@ -40,23 +44,29 @@ func (c *Controller) showLayout(ctx echo.Context) error {
 
 // SetLayout 配信レイアウトの設定
 func (c *Controller) setLayout(ctx echo.Context) error {
+	// パスパラメータからイベントIDを取得
 	eventId := ctx.Param("id")
 
+	// リクエストボディからdomain.SetLayoutRequestを取得
 	params := &domain.SetLayoutRequest{}
 	if err := ctx.Bind(params); err != nil {
 		return ctx.JSON(http.StatusInternalServerError, nil)
 	}
 
-	if err := ctx.Validate(params); err != nil {
+	// リクエストボディのバリデーション
+	err := ctx.Validate(params)
+	if err != nil {
 		return ctx.JSON(http.StatusBadRequest, err.Error())
 	}
 
 	for _, img := range params.Images {
+		// domain.Image.Typeをintに変換
 		t, err := strconv.Atoi(img.Type)
 		if err != nil {
 			return ctx.JSON(http.StatusBadRequest, err.Error())
 		}
 
+		// レイアウトを作成
 		l, err := c.layoutRepository.CreateLayout(t, img.URL, eventId)
 		if err != nil {
 			return err
