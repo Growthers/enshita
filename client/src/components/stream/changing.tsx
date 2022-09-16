fd9546136f74ec31d1a04924b49549f736420f86
sayoi341 <67829283+sayoi341@users.noreply.github.com>
2022-09-17 01:21:03 +0900

    feat(front): add changing page

---
+++ client/src/components/stream/changing.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import TalkInfo from "~/components/stream/talkInfo";
+import { SpeakerProperties } from "~/components/stream/type/model";
+
+import styles from "~/styles/components/stream/changing.module.scss";
+
+type Props = {
+  order: number;
+  speakers: SpeakerProperties[];
+};
+
+const Changing: React.FC<Props> = ({ order, speakers }) => (
+  <div className={styles["enshita-stream-changing"]}>
+    <div className={styles["enshita-stream-changing-next"]}>Next…</div>
+    <div className={styles["enshita-stream-changing-talkInfos"]}>
+      {speakers.slice(0, 3).map(value => (
+        <TalkInfo key={value.id} order={order} speaker={value} />
+      ))}
+    </div>
+  </div>
+);
+
+export default Changing;
