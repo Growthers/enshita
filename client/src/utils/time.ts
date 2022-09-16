140e7cbad7dce491d8982a4de9320935d8345bae
sayoi341 <67829283+sayoi341@users.noreply.github.com>
2022-09-17 01:16:58 +0900

    fix(front): add model

--- client/src/utils/time.ts
+++ client/src/utils/time.ts
@@ -1,8 +1,12 @@
 import dayjs from "dayjs";
+import { ClockProperties } from "~/components/stream/type/model";
 
 export const zeroPadding = (num: number) => `00${num}`.slice(-2);
 
-export const timeDiff = (currentTime: dayjs.Dayjs, targetTime: dayjs.Dayjs) => {
+export const timeDiff = (
+  currentTime: dayjs.Dayjs,
+  targetTime: dayjs.Dayjs,
+): ClockProperties => {
   const diffHour = targetTime.diff(currentTime, "hour");
   const diffMinute = targetTime.diff(currentTime, "minute") - diffHour * 60;
   const diffSecond = targetTime.diff(currentTime, "second") - diffMinute * 60;
