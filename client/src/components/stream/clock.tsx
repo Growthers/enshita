5a485df377db493faeb195c088f9eb46512fb0c4
sayoi341 <67829283+sayoi341@users.noreply.github.com>
2022-09-17 01:21:45 +0900

    fix(front): fix props

--- client/src/components/stream/clock.tsx
+++ client/src/components/stream/clock.tsx
@@ -1,18 +1,15 @@
 import React from "react";
 import styles from "~/styles/components/stream/clock.module.scss";
 import { zeroPadding } from "~/utils/time";
+import { ClockProperties } from "~/components/stream/type/model";
 
 type Props = {
-  time: {
-    first: number;
-    second: number;
-  };
-  css: string | undefined;
+  time: ClockProperties;
 };
 
-const Clock: React.FC<Props> = ({ time, css }) => (
-  <div className={`${styles["enshita-stream-clock"]} ${css}`}>
-    {zeroPadding(time.first)}:{zeroPadding(time.second)}
+const Clock: React.FC<Props> = ({ time: { first, second } }) => (
+  <div className={styles["enshita-stream-clock"]}>
+    {zeroPadding(first)}:{zeroPadding(second)}
   </div>
 );
 export default Clock;
