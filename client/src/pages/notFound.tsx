0870754e341547cb771d519d21c07c7aef992f36
koutyuke <75959529+koutyuke@users.noreply.github.com>
2022-09-17 03:21:03 +0900

    fix(front): do format

--- client/src/pages/notFound.tsx
+++ client/src/pages/notFound.tsx
@@ -2,18 +2,10 @@ import type { FC } from "react";
 import StatusMessage from "~/components/status-message";
 
 const NotFound: FC = () => (
-  <StatusMessage
-    code={404}
-    message="Page Not Found"
-  >
-    <p>
-      Page not implemented or not found.
-    </p>
-    <p>
-      Please check the URL again.
-    </p>
-
+  <StatusMessage code={404} message="Page Not Found">
+    <p>Page not implemented or not found.</p>
+    <p>Please check the URL again.</p>
   </StatusMessage>
-)
+);
 
 export { NotFound };
